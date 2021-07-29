394b6eedc410b74e5ff4fbd0bff363fd7d987d93
Akinolae <makindeakinola22@gmail.com>
2021-07-29 16:29:02 +0100

    intial commit

---
+++ bin/index.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk')
+const boxen = require('boxen')
+const yargs = require('yargs')
+
+const greeting = chalk.white.bold('hello')
+
+const options = yargs
+  .usage(chalk.green.bold('Usage: -n <name>'))
+  .option('n', {
+    alias: 'name',
+    describe: 'auto_dev',
+    type: 'string',
+    demandOption: true,
+  })
+  .option('react', {
+    alias: 'name',
+    describe: 'install react app',
+    type: 'string',
+  }).argv
+
+const boxenStyle = {
+  padding: 1,
+  borderColor: 'white',
+  width: '70%',
+  backgroundColor: 'green',
+}
+
+options.react ? console.log('you picked react') : console.log('waiting')
+const message = boxen(greeting, boxenStyle)
+// console.log(options.name)
